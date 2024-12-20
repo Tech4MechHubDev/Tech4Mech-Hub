ec2c648ce16d16c1a727f2718279549988ce7f25
Tech4MechHubDev <totallynottech4mech@gmail.com>
2024-12-21 00:31:54 +0100

    Update Tech4MechHub.lua

--- Tech4MechHub.lua
+++ Tech4MechHub.lua
@@ -9,7 +9,7 @@ local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHept
 
  local colors = {
         SchemeColor = Color3.fromRGB(86, 76, 251),
-        Background = Color3.fromRGB(26, 32, 58),
+        Background = Color3.fromRGB(44, 62, 82),
         Header = Color3.fromRGB(5, 5, 5),
         TextColor = Color3.fromRGB(200, 200, 200),
         ElementColor = Color3.fromRGB(20, 20, 20)
