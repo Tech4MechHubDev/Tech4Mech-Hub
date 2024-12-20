54b09eeeb3697d69e981640c239f5c4bfe320e62
Tech4MechHubDev <totallynottech4mech@gmail.com>
2024-12-21 00:25:23 +0100

    Update Tech4MechHub.lua

--- Tech4MechHub.lua
+++ Tech4MechHub.lua
@@ -8,14 +8,15 @@ wait(0.1)print("Thx For Using Tech4Mech Script!")
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 
  local colors = {
-    SchemeColor = Color3.fromRGB(0,255,255),
-    Background = Color3.fromRGB(0, 0, 0),
-    Header = Color3.fromRGB(0, 0, 0),
-    TextColor = Color3.fromRGB(255,255,255),
-    ElementColor = Color3.fromRGB(20, 20, 20)
+        SchemeColor = Color3.fromRGB(0, 166, 58),
+        Background = Color3.fromRGB(26, 32, 58),
+        Header = Color3.fromRGB(22, 29, 31),
+        TextColor = Color3.fromRGB(200, 200, 200),
+        ElementColor = Color3.fromRGB(38, 45, 71)
 }
 
-local Window = Library.CreateLib("Tech4Mech HUB|V 1.3", "Serpent")
+local Window = Library.CreateLib("Tech4Mech HUB|V 1.3", colors)
+
  
 local Tab = Window:NewTab("👓 Main")
 local MainUniSection = Tab:NewSection("Universal")
